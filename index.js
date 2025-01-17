a72300af1345fcef5a09f595d37dbf9fca33ecd4
Aaron Kromer <kromer.aaron@gmail.com>
2025-01-17 18:00:33 +0100

    added custom grab

--- index.js
+++ index.js
@@ -1,35 +1,72 @@
-// AFRAME.registerComponent('cursor-listener', {
-//   init: function () {
-//     var lastIndex = -1;
-//     var COLORS = ['red', 'green', 'blue'];
-//     this.el.addEventListener('click', function (evt) {
-//       lastIndex = (lastIndex + 1) % COLORS.length;
-//       this.setAttribute('material', 'color', COLORS[lastIndex]);
-//       console.log('I was clicked at: ', evt.detail.intersection.point);
-//     });
-//   }
-// });
+const vector3ToString = (vector) => {
+  return `${vector.getComponent(0)} ${vector.getComponent(1)} ${vector.getComponent(2)}`;
+}
 
-// AFRAME.registerComponent('abutton-event', {
-//   init: function () {
-//     var lastIndex = -1;
-//     var COLORS = ['red', 'green', 'blue'];
-//     const box = document.getElementById('box');
-//     box.addEventListener('abuttonchanged', function (evt) {
-//       lastIndex = (lastIndex + 1) % COLORS.length;
-//       this.setAttribute('material', 'color', COLORS[lastIndex]);
-//     });
-//   }
-// });
+AFRAME.registerComponent('return-grab', {
+  init: function () {
+    this.system = this.el.sceneEl.systems.physics;
 
-// AFRAME.registerComponent('thumbstick-logging',{
-//   init: function () {
-//     this.el.addEventListener('thumbstickmoved', this.logThumbstick);
-//   },
-//   logThumbstick: function (evt) {
-//     if (evt.detail.y > 0.95) { console.log("DOWN"); }
-//     if (evt.detail.y < -0.95) { console.log("UP"); }
-//     if (evt.detail.x < -0.95) { console.log("LEFT"); }
-//     if (evt.detail.x > 0.95) { console.log("RIGHT"); }
-//   }
-// });
+    this.GRABBED_STATE = 'grabbed';
+
+    this.grabbing = false;
+    this.hitEl =         /** @type {AFRAME.Element}    */ null;
+    this.physics =       /** @type {AFRAME.System}     */ this.el.sceneEl.systems.physics;
+    this.constraint =    /** @type {CANNON.Constraint} */ null;
+
+    // Bind event handlers
+    this.onHit = this.onHit.bind(this);
+    this.onGripOpen = this.onGripOpen.bind(this);
+    this.onGripClose = this.onGripClose.bind(this);
+  },
+
+  play: function () {
+    const el = this.el;
+    el.addEventListener('hit', this.onHit);
+    el.addEventListener('gripdown', this.onGripClose);
+    el.addEventListener('gripup', this.onGripOpen);
+    el.addEventListener('trackpaddown', this.onGripClose);
+    el.addEventListener('trackpadup', this.onGripOpen);
+    el.addEventListener('triggerdown', this.onGripClose);
+    el.addEventListener('triggerup', this.onGripOpen);
+  },
+
+  pause: function () {
+    const el = this.el;
+    el.removeEventListener('hit', this.onHit);
+    el.removeEventListener('gripdown', this.onGripClose);
+    el.removeEventListener('gripup', this.onGripOpen);
+    el.removeEventListener('trackpaddown', this.onGripClose);
+    el.removeEventListener('trackpadup', this.onGripOpen);
+    el.removeEventListener('triggerdown', this.onGripClose);
+    el.removeEventListener('triggerup', this.onGripOpen);
+  },
+
+  onGripClose: function () {
+    this.grabbing = true;
+  },
+
+  onGripOpen: function () {
+    const hitEl = this.hitEl;
+    this.grabbing = false;
+    if (!hitEl) { return; }
+    hitEl.removeState(this.GRABBED_STATE);
+    hitEl.emit(`startReturnTransition`, null, false);
+    this.hitEl = undefined;
+    this.system.removeConstraint(this.constraint);
+    this.constraint = null;
+  },
+
+  onHit: function (evt) {
+    const hitEl = evt.detail.el;
+    // If the element is already grabbed (it could be grabbed by another controller).
+    // If the hand is not grabbing the element does not stick.
+    // If we're already grabbing something you can't grab again.
+    if (hitEl.is(this.GRABBED_STATE) || !this.grabbing || this.hitEl) { return; }
+    const hitElCurrentPos = hitEl.getAttribute('position');
+    hitEl.setAttribute('returnTransition', {'property': 'position', 'to': vector3ToString(hitElCurrentPos), 'startEvents': 'startReturnTransition'});
+    hitEl.addState(this.GRABBED_STATE);
+    this.hitEl = hitEl;
+    this.constraint = new CANNON.LockConstraint(this.el.body, hitEl.body);
+    this.system.addConstraint(this.constraint);
+  }
+});
