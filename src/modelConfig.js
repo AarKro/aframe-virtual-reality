27d863d3d03cb71d0f03d4e5b465ac53cdda18a8
Aaron Kromer <kromer.aaron@gmail.com>
2025-01-25 11:00:02 +0100

    scale models

--- src/modelConfig.js
+++ src/modelConfig.js
@@ -1,22 +1,22 @@
 import { asBox, asCoordinates, patch } from "./utlis.js";
 
 const accessorySlots = {
-  hat: asCoordinates(3.68, 1.95, -2),
-  glasses: asCoordinates(3.2, 1.56, -2),
+  hat: asCoordinates(3.68, 2.05, -2),
+  glasses: asCoordinates(3.2, 1.71, -2),
 }
 
 export const modelConfig = [
   {
     id: 'potato',
     kind: 'environment',
-    position: asCoordinates(4, 1.35, -2),
+    position: asCoordinates(4, 1.5, -2),
     rotation: asCoordinates(0, 90, 0),
   },
   {
     id: 'room',
     kind: 'environment',
     hitbox: asBox(10, 0.5, 10, 0, -5.1, 0),
-    position: asCoordinates(0, 4, 0),
+    position: asCoordinates(0, 4.5, 0),
     rotation: asCoordinates(0, -90, 0),
   },
   // {
@@ -30,8 +30,8 @@ export const modelConfig = [
     id: 'table',
     kind: 'environment',
     hitbox: asBox(2.23, 0.28, 0.385, -0.07, 0.1, -0.03),
-    position: asCoordinates(-5, 0.4, -2),
-    scale: asCoordinates(2, 2, 2),
+    position: asCoordinates(-5, 0.7, -2),
+    scale: asCoordinates(1.7, 1.7, 1.7),
     rotation: asCoordinates(0, 90, 0),
   },
   // {
@@ -44,8 +44,8 @@ export const modelConfig = [
     id: 'stage',
     kind: 'environment',
     hitbox: asBox(0.88, 0.1, 0.87, -0.02, 0.08, -0.18),
-    position: asCoordinates(4.1, 0.52, -1.75),
-    scale: asCoordinates(1.6, 1.6, 1.6),
+    position: asCoordinates(4.1, 0.72, -1.75),
+    scale: asCoordinates(1.3, 1.3, 1.3),
   },
   {
     id: 'hatA',
